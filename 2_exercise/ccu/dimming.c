126bcd1f79961fb8bfe1440529c96330630b944a
ZhishenZ <clemenszhang@gmail.com>
2021-11-05 17:48:51 +0100

    init

---
+++ 2_exercise/ccu/dimming.c
@@ -0,0 +1,21 @@
+#include <xmc_common.h>
+
+void initCCU4(void);
+void connectLED(void);
+
+int main(void) {
+  
+  initCCU4();
+
+  while(1);
+  return 0;
+}
+
+void initCCU4() {
+
+  
+}
+
+void connectLED() {
+}
+
